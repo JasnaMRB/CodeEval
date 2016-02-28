7862e0d7efc25fa13cc73783455eec644d414801
Jasna <JasnaMRB@users.noreply.github.com>
2016-02-27 22:07:09 -0500

    Add FizzBuzz solution
    https://www.codeeval.com/open_challenges/1/

---
+++ FizzBuzz.java
@@ -0,0 +1,31 @@
+package java;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FizzBuzz {
+    public static void main(String[] args) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+        int x, y, n;
+        String[] ary;
+        while ((line = buffer.readLine()) != null) {
+            line = line.trim();
+            ary = line.split(" ");
+            x = Integer.parseInt(ary[0]);
+            y = Integer.parseInt(ary[1]);
+            n = Integer.parseInt(ary[2]);
+            for (int i = 1; i <= n; i++) {
+                if (i % x == 0 && i % y == 0) System.out.print("FB");
+                else if (i % x == 0) System.out.print("F");
+                else if (i % y == 0) System.out.print("B");
+                else System.out.print(i);
+                if (i != n) System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
